8f73aaac56f4522f824acd33438bce57b305ad97
8Mi_Yile <admin@8mi.tech>
2024-02-11 01:37:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import httpx
 import socket
 from flask_caching import Cache
-from flask import Flask, abort, make_response, jsonify
+from flask import Flask, abort, Response
 
 app = Flask(__name__)
 cache = Cache(app)
@@ -23,10 +23,12 @@ def index(link):
     else:
         abort(500)
     
+    # 保存HEAD请求的信息
+    head_response = None
     while True: 
         print(f'HEAD Connection：{full_link}')
         try:
-            response_head = httpx.head(full_link)
+            head_response = httpx.head(full_link)
         except httpx.ConnectError as e:
             request_error_output("HEAD","Connect",e)
         except httpx.RequestError as e:
@@ -34,24 +36,31 @@ def index(link):
         except Exception as e:
             request_error_output("HEAD","Unknown",e)
         
-        if 'Location' not in response_head.headers:
+        if 'Location' not in head_response.headers:
             print(f'GET Connection：{full_link}')
             try:
-                response_get = httpx.get(full_link, timeout=10)
-                flask_response = make_response(response_get.content)
-                flask_response.status_code = response_get.status_code
-                for header, value in response_get.headers.items():
-                    flask_response.headers[header] = value
-                return flask_response
+                response_get = httpx.get(full_link, timeout=10, stream=True)
+                # 生成流式响应
+                def generate():
+                    for chunk in response_get.iter_bytes():
+                        yield chunk
+                
+                # 构造包含HEAD信息的响应
+                response = Response(generate(), mimetype='application/octet-stream')
+                # 添加HEAD请求的响应头部信息到响应中
+                for header, value in head_response.headers.items():
+                    response.headers[header] = value
+                return response
             except httpx.ConnectError as e:
                 request_error_output("GET","Connect",e)
             except httpx.RequestError as e:
                 request_error_output("GET","Request",e)
             except Exception as e:
                 request_error_output("GET","Unknown",e)
+            break
         else:
-            full_link = response_head.headers['Location']
-    
+            full_link = head_response.headers['Location']
+
 def request_error_output(request_type, error_type, error_message):
     print(f'{request_type} / {error_type} Error：{error_message}')
     abort(500)
