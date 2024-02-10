34dd220431d82a1b8e7cf7a101055c09eeeda216
8Mi_Yile <admin@8mi.tech>
2024-02-11 01:04:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,21 @@ cache = Cache(app)
 app.config['CACHE_TYPE'] = 'simple'
 cache.init_app(app)
 
-# 设置自定义的 DNS 服务器地址
-def set_dns(dns_ip):
-    # 使用自定义的 DNS 服务器地址
-    if isinstance(dns_ip, str):
-        dns_ip = [dns_ip]
-    socket.dnsservers = dns_ip
-# 设置自定义 DNS 服务器
-custom_dns = '8.8.8.8'  # 例如，设置 Google Public DNS
-set_dns(custom_dns)
-
 @app.route('/<path:link>')
 @cache.cached(timeout=300)  # 设置缓存过期时间为 300 秒（5 分钟）
 def index(link):
     with httpx.Client() as client:
         if link == 'favicon.ico':
             abort(404)
-        full_link = f"https://{link}" if not link.startswith(('http://', 'https://')) else link
+        
+        temp_link = f"https:/{link}" if not link.startswith(('http:/', 'https:/')) else link # 第一步，安抚Vercel的情绪
+        if 'https:/' in temp_link:
+            full_link = temp_link.replace('https:/', 'https://')
+        elif 'http:/' in temp_link:
+            full_link = temp_link.replace('http:/', 'http://')
+        else:
+            abort(500)
+        
         while True: 
             print(f'HEAD Connection：{full_link}')
             try:
