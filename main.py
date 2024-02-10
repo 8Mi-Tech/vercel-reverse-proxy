7bc14ef9d727026437ebf428a5430a4f1a9b622d
8Mi_Yile <admin@8mi.tech>
2024-02-10 23:53:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-from flask import Flask, redirect
-from flask_caching import Cache
+import os
 import httpx
+from flask_caching import Cache
+from flask import Flask, redirect, jsonify
 
 app = Flask(__name__)
 cache = Cache(app)
-
-Rootlink = "https://rpdl-vercel.8mi.edu.pl/"  # 注意末尾的斜杠，确保根链接以斜杠结尾
-
 app.config['CACHE_TYPE'] = 'simple'
 cache.init_app(app)
-
 @app.route('/<path:link>')
 @cache.cached(timeout=300)  # 设置缓存过期时间为 300 秒（5 分钟）
+
 def index(link):
+    #Rootlink = "https://rpdl-vercel-base.8mi.edu.pl"  # 更改为你的实际代理根链接
     full_link = f"https://{link}" if not link.startswith(('http://', 'https://')) else link
-    response = httpx.head(full_link)
-
-    if 'Location' in response.headers:
-        location = response.headers['Location']
-        # 处理重定向链上的连续重定向
-        while location.startswith(('http://', 'https://')):
-            app.logger.info(f"Redirecting to: {location}")
-            response = httpx.head(location)
-            if 'Location' in response.headers:
-                location = response.headers['Location']
-            else:
-                break  # 如果没有 Location 头，则退出循环
-        return redirect(f"{Rootlink}{location}")  # 在根链接和重定向链接之间添加斜杠
-    else:
-        app.logger.info(f"No 'Location' header found in response for link: {link}")
-        return redirect(f"{Rootlink}/{link}")  # 在根链接和路径之间添加斜杠
-
+    while True: 
+      response = httpx.head(full_link)
+      if 'Location' not in response.headers:
+          return response
+      else:
+          full_link = response.headers['Location']
+    
 if __name__ == "__main__":
     app.run()
